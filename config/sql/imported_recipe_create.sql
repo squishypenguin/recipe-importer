c79d3ee6d0159d0c8033e4e4d35a63cc0457ee5a
Julie Mather <jcmather21@yahoo.com>
2016-04-05 13:55:38 -0700

    forgot the update to the imported_recipe table

--- config/sql/imported_recipe_create.sql
+++ config/sql/imported_recipe_create.sql
@@ -3,10 +3,12 @@ CREATE TABLE `imported_recipe` (
   `name` varchar(255) NOT NULL,
   `attributes_blob` blob,
   `ingredients_blob` blob NOT NULL,
+  `ingredients_tags` varchar(1000) NOT NULL,
   `directions_blob` blob NOT NULL,
   `notes` varchar(1024) DEFAULT NULL,
+  `url_domain` varchar(100) DEFAULT NULL,
   `url` varchar(500) NOT NULL,
   `created_timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `url_UNIQUE` (`url`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `unique_recipe_index` (`name`,`url`)
+) ENGINE=InnoDB AUTO_INCREMENT=243 DEFAULT CHARSET=utf8;
